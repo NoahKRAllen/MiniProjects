9d655d923984094d89482cb9398e2c49f231b7ab
Noah Allen <NoahKRAllen@gmail.com>
2025-08-17 21:26:46 -0700

    Working collision target movement

---
+++ BouncingBalls/Assets/Scripts/MovingTargetController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MovingTargetController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject movingTargetOne, movingTargetTwo;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movingTargetOne.transform.position = movingTargetOne.transform.position.x <= -18 ? new Vector3(18, 0, 0) : 
+            new Vector3(movingTargetOne.transform.position.x - Time.deltaTime, 0, 0);
+        
+        movingTargetTwo.transform.position = movingTargetTwo.transform.position.x <= -18 ? new Vector3(18, 0, 0) : 
+            new Vector3(movingTargetTwo.transform.position.x - Time.deltaTime, 0, 0);
+        
+        //Debug.Log(movingTargetOne.transform.position);
+        //Debug.Log(movingTargetTwo.transform.position);
+    }
+}
